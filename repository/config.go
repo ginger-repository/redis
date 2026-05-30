c2ad4ca7430300303b484e61b4d3b5107d0114a2
Saeed Kamyabi <saeed@kamyabi.me>
2026-05-30 19:55:09 +0330

    password file

--- repository/config.go
+++ repository/config.go
@@ -1,16 +1,27 @@
 package repository
 
-import "time"
+import (
+	"os"
+	"time"
+)
 
 type config struct {
-	Addr     string
-	Password string
-	DB       int
-	Timeout  time.Duration
+	Addr         string
+	Password     string
+	PasswordFile string
+	DB           int
+	Timeout      time.Duration
 }
 
 func (c *config) initialize() {
 	if c.Timeout == 0 {
 		c.Timeout = time.Second * 15
+	}
+	if c.Password == "" && c.PasswordFile != "" {
+		password, err := os.ReadFile(c.PasswordFile)
+		if err != nil {
+			panic(err)
+		}
+		c.Password = string(password)
 	}
 }
